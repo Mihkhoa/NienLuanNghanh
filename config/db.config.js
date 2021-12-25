dc73e34b22b2871dba53bd0e0d64f818c93a51fc
Mihkhoa <41445492+Mihkhoa@users.noreply.github.com>
2021-12-25 09:27:14 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "containers-us-west-21.railway.app",
+  HOST: "containers-us-west-21.railway.app:5865",
   USER: "root",
   PASSWORD: "QdEM63PR9CQHhbQSw3lD",
   DATABASE: "railway",
